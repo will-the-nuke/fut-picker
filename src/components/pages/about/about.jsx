53584afad7906d108f11bf7a2c5abf8f81366aba
will-the-nuke <wrstrem@gmail.com>
2024-09-20 11:48:53 +0100

    template

---
+++ src/components/pages/about/about.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+
+    constructor() {
+        this.state = {}
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+            </React.Fragment>
+        );
+    };
+};
+
+export default About;
