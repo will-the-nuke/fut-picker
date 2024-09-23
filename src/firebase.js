9ccda3696a376906359cfb788117e6c7d449dabe
will-the-nuke <wrstrem@gmail.com>
2024-09-23 10:55:50 +0100

    firebase & .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
 //firebase init file
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "fut-picker.firebaseapp.com",
+  projectId: "fut-picker",
+  storageBucket: "fut-picker.appspot.com",
+  messagingSenderId: "187651233889",
+  appId: "1:187651233889:web:3413d6b87e37cb744cb220"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
