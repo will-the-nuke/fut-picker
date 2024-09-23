5edd30ec08aa58574957ab5d1930843e8725e3a1
Will Rutland <wrutland07@gmail.com>
2024-09-23 18:56:51 +0100

    made header show all pages, regardless of user's current page

--- src/components/multiPageComponents/header.jsx
+++ src/components/multiPageComponents/header.jsx
@@ -14,14 +14,6 @@ class Header extends Component {
 
     componentDidMount() {
 
-        //NEED TO MAKE THIS REFRESH ON PAGE CHANGE
-        
-        //get the current page the user is on
-        let currentPage = window.location.href;
-
-        //cuts current page to be only the text after the last / of the url (the useful bit)
-        currentPage = currentPage.substring(currentPage.lastIndexOf('/') + 1);
-
         const pages = [
             ['', 'Home'],
             ['about', 'About'],
@@ -33,22 +25,18 @@ class Header extends Component {
         //repeat for each page
         pages.forEach(([backendName, frontendName]) => {
 
-            //make the that we do not add the user's current page to the header
-            if (backendName != currentPage) {
-
-                //generate HTML for this header cell
-                headerHTML.push(
-                    <td className="headerCell">
-                        <div className="innerHeaderCell">
-                            <Link to={'/' + backendName} style={{height: '100%'}}>
-                                <h3 className="headerCellText">
-                                    {frontendName}
-                                </h3>
-                            </Link>
-                        </div>
-                    </td>
-                );
-            };
+            //generate HTML for this header cell
+            headerHTML.push(
+                <td className="headerCell">
+                    <div className="innerHeaderCell">
+                        <Link to={'/' + backendName} style={{height: '100%'}}>
+                            <h3 className="headerCellText">
+                                {frontendName}
+                            </h3>
+                        </Link>
+                    </div>
+                </td>
+            );
         });
 
         //save header HTML to state
