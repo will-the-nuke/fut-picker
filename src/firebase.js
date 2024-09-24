42029bba5b3a3961a8e9d62c77fc1f4569a7a3cf
Will Rutland <wrutland07@gmail.com>
2024-09-24 07:47:27 +0100

    re-jigged firebase file

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,5 @@
 //firebase init file
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +12,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
